1d20d08c1458626df41b299ea9f77cfc5526e41d
YASH SINGH <singhyash9009670@gmail.com>
2025-01-25 14:36:21 +0530

    added admin pages and protected the admin routes. fixed some minoe errors.

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,29 @@
 import { NextResponse } from "next/server";
-
-export function middleware(request) {
+import { jwtVerify } from "jose";
+export async function middleware(request) {
   const path = request.nextUrl.pathname;
   const isPublicPath =
     path === "/login" ||
     path === "/signup" ||
     path === "/verifyemail" ||
-    path === "/resetPass"|| path==="/";
+    path === "/resetPass" ||
+    path === "/";
+  const adminPath = path.slice(0, 6) === "/admin";
   const token = request.cookies.get("token")?.value || "";
+  // Admin Role Protected Route
+  if (token && adminPath) {
+    function getJwtSecretKey() {
+      const secret = process.env.JWT_SECRET;
+      if (!secret) {
+        throw new Error("JWT Secret key is not matched");
+      }
+      return new TextEncoder().encode(secret);
+    }
+    const { payload } = await jwtVerify(token, getJwtSecretKey());
+    if (!payload.isAdmin) {
+      return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
+  }
   if (isPublicPath && token && path !== "/") {
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
@@ -21,10 +37,11 @@ export const config = {
   matcher: [
     "/profile",
     "/logout",
+    "/admin",
     "/",
     "/login",
     "/signup",
     "/verifyemail",
-    "/resetPass",
+    "/resetPass/:path",
   ],
 };

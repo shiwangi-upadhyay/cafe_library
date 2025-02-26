d9d04b5e4660cac1b7e01a46c9cba1e4c8799dde
YASH SINGH <singhyash9009670@gmail.com>
2025-02-26 18:59:17 +0530

    testing 3.0

--- src/app/api/users/verifyPay/route.js
+++ src/app/api/users/verifyPay/route.js
@@ -1,5 +1,7 @@
+import { connect } from "@/dbConfig/dbConfig";
 import Rented from "@/models/rentedModel";
 import { NextResponse } from "next/server";
+connect();
 export async function POST(request) {
   const body = await request.body();
   const rented= new Rented({username:"yash singh testing"})

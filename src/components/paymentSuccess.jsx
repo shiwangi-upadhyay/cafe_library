07aa25abaf17c7d6f3d7c9240d2431152d71d031
YASH SINGH <singhyash9009670@gmail.com>
2025-03-17 16:40:00 +0530

    Payment Page

--- src/components/paymentSuccess.jsx
+++ src/components/paymentSuccess.jsx
@@ -2,9 +2,10 @@
 import React, { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { useGetRentedBookByPaymentIdMutation } from "@/redux/services/userApi";
-
+import { client } from "@/sanity/lib/client";
+import { RiStockFill } from "react-icons/ri";
 const PaymentSuccess = () => {
-  const [getData, { data}] = useGetRentedBookByPaymentIdMutation();
+  const [getData, { data }] = useGetRentedBookByPaymentIdMutation();
   const searchParams = useSearchParams();
   const razorpay_payment_id = searchParams.get("razorpay_payment_id");
   useEffect(() => {
@@ -12,10 +13,6 @@ const PaymentSuccess = () => {
   }, []);
   return (
     <div>
-      <h1>Payment Successful</h1>
-      <p>
-        <strong>Razorpay Payment ID:</strong> {razorpay_payment_id}
-      </p>
       <div>
         <h1>Book: {data?.bookTitle}</h1>
         <p>Franchise: {data?.franchiseName}</p>
